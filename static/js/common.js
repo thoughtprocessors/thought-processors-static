9b9276dec7b6631a754b32bb4c59629d69a52752
Ishan Manchanda <ishanmanchanda70@gmail.com>
2021-04-22 21:19:34 +0530

    Added team and index pages + static files

---
+++ static/js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	$('.button-collapse').sideNav({
+		menuWidth: 300,
+		draggable: true,
+	});
+
+	new WOW().init();
+});
---
+++ static/js/index.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+	$('.banner').css('height', window.innerHeight + 'px');
+});
